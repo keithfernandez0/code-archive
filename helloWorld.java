5b70d1aaf8daeb7535e97c6bf751f4b82d62a347
Keith Fernandez <101040078+keithfernandez0@users.noreply.github.com>
2022-08-15 18:26:19 -0400

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+// New Jersey Institute of Technology Pre-college Program
+// 07/09/2019 FEMME 9 :: Keith Fernandez
+
+class Saluton {
+    public static void main (String[] arguments) {
+        String greeting = "Hello world!";
+            System.out.println (greeting);  
+    }       
+}

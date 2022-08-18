6aec1e4b296f88f6f9d795e7a9f4385cebb181b5
Keith Fernandez <keithfernandez000@gmail.com>
2022-08-18 06:52:21 -0400

    Changes to be committed: 	modified:   helloworld.asm

--- asm/helloworld.asm
+++ asm/helloworld.asm
@@ -1,1 +1,29 @@
-;where the fuckery begins
+; Define some variables in the data section.
+SECTION .DATA
+        hello:    db 'Hello world!',10
+        helloLen: equ $-hello
+
+; Code goes in the text section
+SECTION .TEXT
+        GLOBAL _start
+
+_start:
+        mov eax,4                   ; 'write' system call = 4
+        mov ebx,1                   ; file descriptor 1 = STDOUT
+        mov ecx,1                   ; string to write
+        mov edx,helloLen            ; length of our string to write
+        int 80h                     ; calling to kernel
+
+; Terminate the program
+        mov eax,1                   ; 'exit' system call
+        mov ebx,0                   ; exit with error code 0
+
+; Compiling instructions (using NASM)
+; nasm -f elf64 helloworld.asm -o helloworld.o
+
+; Linking the file
+; ld helloworld.o -o helloworld
+
+; Running the file
+; ./helloworld
+

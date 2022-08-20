8a070164b2c4fea9b6b4d256a782de79af61f388
Keith Fernandez <keithfernandez000@gmail.com>
2022-08-20 02:52:10 -0400

    adding some more comments/organization to better understand the file

--- asm/helloworld.asm
+++ asm/helloworld.asm
@@ -1,29 +1,33 @@
 ; Define some variables in the data section.
+
 SECTION .DATA
-        hello:    db 'Hello world!', 10
-        helloLen: equ $-hello
+	hello:    db 'Hello world!', 10	     ; db = define byte
+        helloLen: equ $-hello		     ; equ = setting absolute value (string length)
 
 ; Code goes in the text section
+
 SECTION .TEXT
         GLOBAL _start
 
 _start:
-        mov eax, 4                   ; 'write' system call = 4
-        mov ebx, 1                   ; file descriptor 1 = STDOUT
-        mov ecx, 1                   ; string to write
-        mov edx, helloLen            ; length of our string to write
-        int 80h                     ; calling to kernel
+        mov eax, 4	                     ; 'write' system call = 4
+        mov ebx, 1      	             ; file descriptor 1 = STDOUT
+        mov ecx, 1              	     ; string to write
+        mov edx, helloLen           	     ; length of our string to write
+        int 80h                    	     ; calling to kernel
 
 ; Terminate the program
-        mov eax, 1                   ; 'exit' system call
-        mov ebx, 0                   ; exit with error code 0
+        mov eax, 1                  	     ; 'exit' system call
+        mov ebx, 0                  	     ; exit with error code 0
 
-; Compiling instructions (using NASM)
+; ==========================================================================
+
+; COMMAND FOR COMPILING USING NASM
 ; nasm -f elf64 helloworld.asm -o helloworld.o
 
-; Linking the file
+; LINKING FILE
 ; ld helloworld.o -o helloworld
 
-; Running the file
+; FILE EXECUTION
 ; ./helloworld
 

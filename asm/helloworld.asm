02be81fb6be81b4ae6a7e94002568aa175efc7dd
Keith Fernandez <keithfernandez000@gmail.com>
2022-08-19 23:12:31 -0400

    added some extra spaces to helloworld.asm file to make it prettier

--- asm/helloworld.asm
+++ asm/helloworld.asm
@@ -1,6 +1,6 @@
 ; Define some variables in the data section.
 SECTION .DATA
-        hello:    db 'Hello world!',10
+        hello:    db 'Hello world!', 10
         helloLen: equ $-hello
 
 ; Code goes in the text section
@@ -8,15 +8,15 @@ SECTION .TEXT
         GLOBAL _start
 
 _start:
-        mov eax,4                   ; 'write' system call = 4
-        mov ebx,1                   ; file descriptor 1 = STDOUT
-        mov ecx,1                   ; string to write
-        mov edx,helloLen            ; length of our string to write
+        mov eax, 4                   ; 'write' system call = 4
+        mov ebx, 1                   ; file descriptor 1 = STDOUT
+        mov ecx, 1                   ; string to write
+        mov edx, helloLen            ; length of our string to write
         int 80h                     ; calling to kernel
 
 ; Terminate the program
-        mov eax,1                   ; 'exit' system call
-        mov ebx,0                   ; exit with error code 0
+        mov eax, 1                   ; 'exit' system call
+        mov ebx, 0                   ; exit with error code 0
 
 ; Compiling instructions (using NASM)
 ; nasm -f elf64 helloworld.asm -o helloworld.o
